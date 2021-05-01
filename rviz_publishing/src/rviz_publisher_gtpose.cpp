cb49aebb9bb32cc173142719344d61d271db1911
Lorenzo <lorenzo2.cassani@mail.polimi.it>
2021-05-01 19:39:07 +0200

    Aggiornamento gt_pose
    Ho tolto la riga che faceva partire la bag dal launcher (ahimè è l'unica soluzione per non avere conflitti), ho moficato a posto gt_pose_tf (ora è identico a gt_pose, senza compensazioni) e cambiato gt_pose_tf in una pose, che prima era un odometry

--- rviz_publishing/src/rviz_publisher_gtpose.cpp
+++ rviz_publishing/src/rviz_publisher_gtpose.cpp
@@ -15,7 +15,7 @@
 class rviz_publisher_gtpose
 {
 
-    nav_msgs::Odometry messaggio;
+    geometry_msgs::PoseStamped messaggio;
 private:
     ros::NodeHandle n;
     tf::TransformBroadcaster br;
@@ -27,7 +27,7 @@ public:
      rviz_publisher_gtpose(){
 
   	sub = n.subscribe("/gt_pose", 200, &rviz_publisher_gtpose::callback, this);
-  	pub = n.advertise<nav_msgs::Odometry>("/gt_pose_tf", 200);
+  	pub = n.advertise<geometry_msgs::PoseStamped>("/gt_pose_tf", 200);
   	         while (ros::ok()){
              ros::spin();
          }
@@ -40,15 +40,15 @@ void callback(const geometry_msgs::PoseStamped::ConstPtr &msg){
 
     messaggio.header.stamp = ros::Time::now();
     messaggio.header.frame_id = "map";
-    messaggio.child_frame_id = "gtpose";
+    //messaggio.header.child_frame_id = "gtpose";
 
-    messaggio.pose.pose.position.x = msg->pose.position.x+0.832142114639;
-    messaggio.pose.pose.position.y = msg->pose.position.y-0.426361680031;
-    messaggio.pose.pose.position.z= 0;
-    messaggio.pose.pose.orientation.x = msg->pose.orientation.x-0.0221652016044;
-    messaggio.pose.pose.orientation.y = msg->pose.orientation.y+0.0104456143454;
-    messaggio.pose.pose.orientation.z = msg->pose.orientation.z+0.533567428589;
-    messaggio.pose.pose.orientation.w = msg->pose.orientation.w+0.1545997461224;
+    messaggio.pose.position.x = msg->pose.position.x;
+    messaggio.pose.position.y = msg->pose.position.y;
+    messaggio.pose.position.z= 0;
+    messaggio.pose.orientation.x = msg->pose.orientation.x;
+    messaggio.pose.orientation.y = msg->pose.orientation.y;
+    messaggio.pose.orientation.z = msg->pose.orientation.z;
+    messaggio.pose.orientation.w = msg->pose.orientation.w;
 
     /*messaggio.twist.twist.linear.x = msg->twist.twist.linear.x;
     messaggio.twist.twist.linear.y = msg->twist.twist.linear.y;
@@ -59,12 +59,12 @@ void callback(const geometry_msgs::PoseStamped::ConstPtr &msg){
     messaggio.twist.twist.angular.z = msg->twist.twist.angular.z;*/
 
 
-    transform.setOrigin( tf::Vector3(msg->pose.position.x+0.832142114639, msg->pose.position.y-0.426361680031, 0) );
+    transform.setOrigin( tf::Vector3(msg->pose.position.x, msg->pose.position.y, 0) );
         tf::Quaternion q;
-        q[0] = msg->pose.orientation.x-0.0221652016044;
-        q[1] = msg->pose.orientation.y+0.0104456143454;
-        q[2] = msg->pose.orientation.z+0.533567428589;
-        q[3] = msg->pose.orientation.w+0.1545997461224;
+        q[0] = msg->pose.orientation.x;
+        q[1] = msg->pose.orientation.y;
+        q[2] = msg->pose.orientation.z;
+        q[3] = msg->pose.orientation.w;
         transform.setRotation(q);
         br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "map", "gtpose"));
 
