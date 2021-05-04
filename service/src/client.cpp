8438abfcdd5c2ba166eb6defa6942a5966691479
DavideCanzi <davide.canzi@mail.polimi.it>
2021-05-04 11:09:06 +0200

    Patch client service
    ora il client funziona, ma con given_pose fa casino

--- service/src/client.cpp
+++ service/src/client.cpp
@@ -5,6 +5,10 @@
 char input;
 
 std::string coordinate;
+float coordinata_x;
+float coordinata_y;
+float coordinata_theta;
+
 int main(int argc, char **argv)
 {
 
@@ -15,20 +19,31 @@ int main(int argc, char **argv)
   ros::ServiceClient client1 = n.serviceClient<service::GivenPose>("given_pose");
   service::ResetZero srv1;
   service::GivenPose srv2;
-  std::cout << "Ready to change the odometry, press enter to reset to origin or x,y,theta to set coordinates..";
+  std::cout << "Ready to change the odometry, write '1' to reset to origin or '2' to set coordinates..";
   std::cin >> input;
-  if(input == '\n'){
-      srv1.request.x = 0;
-      srv1.request.y = 0;
-      srv1.request.theta = 0;
-      ROS_INFO("CAMBIATO ALL'ORIGINE");
+  if(input == '1'){
+      if (client.call(srv1)) {
+          srv1.request.x = 0;
+          srv1.request.y = 0;
+          srv1.request.theta = 0;
+          ROS_INFO("CAMBIATO ALL'ORIGINE");
+      }
   }
-  else{
-      std::cout << "Ready to take the desider coordinates: ";
-      srv2.request.x = coordinate[0];
-      srv2.request.y = coordinate[2];
-      srv2.request.theta = coordinate[4];
-      ROS_INFO("CAMBIATO ALLA POSIZIONE DESIDERATA");
+  else if (input == '2'){
+      //std::cout << "Ready to take the desider coordinates: ";
+      if (client.call(srv2)) {
+          std::cout << "x coordinate: ";
+          std::cin >> coordinata_x;
+          std::cout << "y coordinate: ";
+          std::cin >> coordinata_y;
+          std::cout << "theta coordinate: ";
+          std::cin >> coordinata_theta;
+
+          srv2.request.x = coordinata_x;
+          srv2.request.y = coordinata_y;
+          srv2.request.theta = coordinata_theta;
+          ROS_INFO("CAMBIATO ALLA POSIZIONE DESIDERATA");
+      }
 
   }
 
